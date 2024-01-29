d115ea1d00954708e8134a0d880c26884cb9fdf8
Anmol Srivastava <anmolsrivastava@Anmols-MacBook-Pro.local>
2024-01-29 18:44:35 +0530

    fix postgres config

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 const { Pool } = require('pg');
 const pgClient = new Pool({
     user: keys.pgUser,
-    host: keys.pgUser,
+    host: keys.pgHost,
     database: keys.pgDatabase,
     password: keys.pgPassword,
     port: keys.pgPort
